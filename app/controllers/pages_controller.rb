bf2061a0a79c72da1b307b856255d484bf4ec86c
Diego Anunciacao <diegoeliasanunciacao@gmail.com>
2025-05-12 21:41:50 +0100

    home page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def home
+    @ICONS = [
+      { icon: 'camera', count: '500+', text: 'Weddings Captured' },
+      { icon: 'heart', count: '16+', text: 'Years of Experience' },
+      { icon: 'pin', count: '12', text: 'European Countries' },
+      { icon: 'calendar', count: '100%', text: 'Happy Couples' }
+    ]
+
+    @COVERS = [
+      { image: "HELEN.jpg" },
+      { image: "Wedding.jpg" },
+      { image: "WEDDING1.jpg" }
+    ]
+  end
+end
---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,38 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["menu", "openIcon", "closeIcon", "navbar"]
+
+  connect() {
+    this.open = false
+    this._update()
+    window.addEventListener("scroll", this.handleScroll.bind(this))
+  }
+
+  disconnect() {
+    window.removeEventListener("scroll", this.handleScroll.bind(this))
+  }
+
+  toggle() {
+    this.open = !this.open
+    this._update()
+  }
+
+  _update() {
+    this.menuTarget.classList.toggle("hidden", !this.open)
+    this.openIconTarget.classList.toggle("hidden", this.open)
+    this.closeIconTarget.classList.toggle("hidden", !this.open)
+  }
+
+  handleScroll() {
+    const scrollY = window.scrollY
+
+    if (scrollY > 10) {
+      this.navbarTarget.classList.add("bg-white", "shadow", "py-3")
+      this.navbarTarget.classList.remove("bg-transparent", "py-5")
+    } else {
+      this.navbarTarget.classList.remove("bg-white", "shadow", "py-3")
+      this.navbarTarget.classList.add("bg-transparent", "py-5")
+    }
+  }
+}
